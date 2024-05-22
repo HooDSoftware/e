7ba7cb8cd95d107696202433f88463e88e3cfe75
HooDSoftwares <108322650+HooDSoftware@users.noreply.github.com>
2024-05-22 11:13:37 +0300

    Add files via upload

---
+++ lib/minor_screens/subcateg_products.dart
@@ -0,0 +1,26 @@
+import "package:flutter/material.dart";
+import "package:multi_store_app/widgets/appbar_widgets.dart";
+
+class SubCategProducts extends StatelessWidget {
+  final String subcategName;
+  final String maincategName;
+
+  const SubCategProducts({Key? key, required this.subcategName, required this.maincategName}): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar:AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading: const AppBarBackButton(
+
+        ),
+        title:AppBarTitle(title: subcategName, )
+        ),
+        body:SafeArea(child: Center(child:Text(maincategName),))
+      );
+  
+  }
+}
+
